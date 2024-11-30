c742859b1eeba148676ff42bb0d2ac5e1cf3c6b8
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-11-30 12:26:46 +0600

    Initialized the project

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello, Node learner!");
+});
+
+app.listen(8080, () => {
+  console.log("The port is running on 8080");
+});
